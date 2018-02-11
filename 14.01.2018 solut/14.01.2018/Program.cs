ccf895425b3e05907cf2ba106920d6bab0c35ed8
Ali Allahverdiyev <32015778+AliAllahverdi@users.noreply.github.com>
2018-02-11 19:19:53 +0400

    Add files via upload

---
+++ 14.01.2018 solut/14.01.2018/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._01._2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =5;
+            a -= -5;
+            int b = 4;
+            Console.WriteLine(a+b);
+        }
+    }
+}
