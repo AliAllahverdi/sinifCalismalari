fcbda0f2fb755dc79612ee67d4b5fb8e5dcb90b0
Ali Allahverdiyev <32015778+AliAllahverdi@users.noreply.github.com>
2018-02-11 19:19:11 +0400

    Add files via upload

---
+++ 15ci hefte/consolApp/consolApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            /*
+            int quvvet(int a, int b)
+            {
+                int s = 1;
+                for (int i = 1; i <= b; i++)
+                {
+                    s = s * a;
+                }
+                return s;
+            }
+            Console.WriteLine("ededi daxil edin:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("ededin quvvetini daxil edin:");
+            int y = int.Parse(Console.ReadLine());
+            int d = quvvet(x, y);
+            Console.WriteLine(d);
+            Console.ReadKey();*/
+
+
+
+        }
+
+       
+}
+}
+
+
+
---
+++ 15ci hefte/index2/index2/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* var a = int.Parse(Console.ReadLine());
+              Console.WriteLine(firstFunction(a));
+             string firstFunction(int t)
+              {
+
+                  return "7" + t.ToString() + "8"; // "5558"
+              }*/
+
+            //----------------------------------------------------------------  2  -------------------------------------------------------
+            /* var b = int.Parse(Console.ReadLine());
+             Console.WriteLine(firstFunction(b));
+             string firstFunction(int t)
+             {
+                 return t.ToString() + t.ToString();
+             }*/
+
+            //----------------------------------------------------------------  3 -------------------------------------------------------
+            /*Console.WriteLine(functionThree(int.Parse(Console.ReadLine())));
+                
+            int functionThree(int eded)
+            {
+                eded = eded * 18 / 100;
+                eded = eded * 3 / 100;
+
+                return eded;
+               
+            }
+
+            Console.ReadLine();*/
+            //----------------------------------------------------------------  4  -------------------------------------------------------
+
+            /*  var a = int.Parse(Console.ReadLine());
+              Console.WriteLine(fourFunction(a));
+              string fourFunction(int t)
+              {
+                  var s = t.ToString() + "7";
+                  var c = int.Parse(s);
+                  c = c * 7 / 100;
+
+                  return c.ToString();
+              }
+
+              Console.WriteLine(fourFunction(int.Parse(Console.ReadLine())));
+
+
+              Console.ReadLine(); */
+
+            //----------------------------------------------------------------  5  -------------------------------------------------------
+
+            /* var eded = int.Parse(Console.ReadLine());
+             Console.WriteLine(myFunction(eded));
+             string myFunction(int a)
+             {
+                 var s = "4" + a.ToString() + "44";
+                 var c = int.Parse(s);
+                 c = c * 44 / 100;
+
+                 return c.ToString();
+             }
+
+             Console.WriteLine(myFunction(int.Parse(Console.ReadLine())));
+
+
+             Console.ReadLine(); */
+
+
+            //----------------------------------------------------------------  6  -------------------------------------------------------
+
+
+            /* Console.WriteLine(functionThree(int.Parse(Console.ReadLine())));
+
+             int functionThree(int eded)
+             {
+                 eded = eded * 20 / 100;
+                 eded = eded * 10 / 100;
+                 eded = eded * 2;
+                 return eded;
+
+             }
+
+             Console.ReadLine();*/
+
+            //----------------------------------------------------------------  7  -------------------------------------------------------
+
+            /* int a = int.Parse(Console.ReadLine());
+             int b = int.Parse(Console.ReadLine());
+             Console.WriteLine(firstFunction(a+b));
+             string firstFunction(int t)
+             {
+
+                 var x= "5" + t.ToString() + "5";
+                 var y = int.Parse(x);
+                 y = y * 5 / 100;
+
+                 return y.ToString();
+
+             }
+             Console.ReadLine(); */
+
+            //----------------------------------------------------------------  8 -------------------------------------------------------
+
+            /*  int h = int.Parse(Console.ReadLine());
+              int j = int.Parse(Console.ReadLine());
+              Console.WriteLine(myFunction(h, j));
+              int myFunction(int a, int b)
+              {
+                  var x = a * 4 / 100;
+                  var y = b * 9 / 100;
+                  var c = (x + y)*10/100;
+                  return c; 
+              } */
+
+            //----------------------------------------------------------------  9  -------------------------------------------------------
+
+            //int a = int.Parse(Console.ReadLine());
+            //  int s = int.Parse(Console.ReadLine());
+            //  int d = int.Parse(Console.ReadLine());
+            //  Console.WriteLine(nineFunction(a, s, d));
+
+            //  int nineFunction(int z, int x, int c)
+            //  {
+
+            //      var h = z * 10 / 100;
+            //      var j = x * 10 / 100;
+            //      var k = c * 10 / 100;
+            //      var l = (h + j + k) * 10 / 100;
+
+            //      return l;
+            //  }
+
+
+            //----------------------------------------------------------------  10 -------------------------------------------------------
+
+            //int a = int.Parse(Console.ReadLine());
+            //int s = int.Parse(Console.ReadLine());
+            //int d = int.Parse(Console.ReadLine()); //"555"
+            //Console.WriteLine(tenFunction(a, s, d));
+
+            //int tenFunction(int z, int x, int c)
+
+            //{
+            //    var h = z * 1 / 100;
+            //    var j = x * 2 / 100;
+            //    var k = c * 3 / 100;
+            //    var p = c * 7 / 100;
+            //    var l = (h - j - k);
+
+            //    return l+p;
+            //}
+
+
+
+            //----------------------------------------------------------------  11 -------------------------------------------------------
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+            //int d = int.Parse(Console.ReadLine());
+            //Console.WriteLine(myFunction(a,b,c,d));
+
+            //int myFunction(int h, int j, int k, int l)
+            //{
+            //    var v = h + k;
+            //    var m = j + l;
+            //    var p = k * 3 / 100;
+            //    var t = v * m;
+            //    return t - p;
+            //}
+
+
+
+           
+
+        }
+    }
+}
