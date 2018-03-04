2deae60fb82c19ee55e561108a891e700084e575
Ali Allahverdiyev <32015778+AliAllahverdi@users.noreply.github.com>
2018-03-04 19:51:09 +0400

    Add files via upload

---
+++ ViewDers/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewDers.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Ana Sehifedesiz";
+            return View();
+        }
+       
+        public ActionResult Show()
+        {
+       
+            return View();
+        }
+        public string Show2(string myText)
+        {
+            StreamWriter writer = new StreamWriter(@"C:\Users\P502\Desktop\ali\ViewDers\tst.txt",append:true);
+            
+                writer.WriteLine(myText);
+                writer.Close();
+
+            StreamReader read = new StreamReader(@"C:\Users\P502\Desktop\ali\ViewDers\tst.txt");
+            var s = read.ReadToEnd();
+            read.Close();
+            return s;
+        }
+        
+        public ActionResult About()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ ViewDers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewDers.MvcApplication" Language="C#" %>
