e76f11148523dc20721ea7963a8ef88c41c56698
Ali Allahverdiyev <32015778+AliAllahverdi@users.noreply.github.com>
2018-03-18 19:48:49 +0400

    Add files via upload

---
+++ ListApp/ListApp/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListApp.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string userid { get; set; }
+        public DateTime deadline { get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+
+        public virtual User userTask { get; set; }
+    }
+}
---
+++ ListApp/ListApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ListApp.Models
+{  
+        public class User
+        {
+            public int id { get; set; }
+            public string userName { get; set; }
+
+            [MinLength(8)]
+            [DataType(DataType.Password)]
+            public string parol { get; set; }
+
+            public bool isAdmin { get; set; }
+        }
+}
