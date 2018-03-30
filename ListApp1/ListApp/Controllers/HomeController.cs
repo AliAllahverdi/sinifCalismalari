6a90a7df8b5e91aa5753c8306f06a0c6a13eea11
Ali Allahverdiyev <32015778+AliAllahverdi@users.noreply.github.com>
2018-03-30 13:24:03 +0400

    Add files via upload

---
+++ ListApp1/ListApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using ListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DBmodel db = new DBmodel();
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(newUser);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            
+           
+            return RedirectToAction("SignUp");
+            
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var tapilan = db.Users.FirstOrDefault(f => f.userName == user.userName && f.parol == user.parol);
+               
+
+                if (tapilan == null)
+                {
+                    return View();
+                }
+                else
+                {
+                    Session["userName"] = tapilan.userName;
+                    Session["isadmin"] = tapilan.isAdmin;
+                    Session["status"] = true;
+                }
+
+                return RedirectToAction("Index", "Task");
+            }
+
+            return View();
+
+
+        }
+    }
+}
---
+++ ListApp1/ListApp/Controllers/TaskController.cs
@@ -0,0 +1,59 @@
+﻿using ListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        DBmodel db = new DBmodel();
+        public ActionResult Index()
+        {
+           if(Session["status"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                var tasklist = db.Tasks.Where(w=>w.userid==Session)
+                return View()
+            }
+           
+        }
+        // GET: Task
+        public ActionResult Create()
+        {
+            if ((bool)Session["isadmin"] == true)
+            {
+return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            if ((bool)Session["isadmin"] == true)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
